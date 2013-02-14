9aa179693aac7c4e77d6af60f093370912e7a604
Jarl Stefansson <jarl.stefansson@gmail.com>
2013-02-14 10:26:21 -0500

    Added bat script to create symlinks for windows, should change ghar at some point to support windows.

---
+++ WindowsSymlinks.bat
@@ -0,0 +1,6 @@
+REM echo %USERPROFILE%
+mklink /D %USERPROFILE%\.vim %USERPROFILE%\Revisions\ghar\home\.vim
+mklink /D %USERPROFILE%\vimfiles %USERPROFILE%\Revisions\ghar\home\.vim
+mklink %USERPROFILE%\.vimrc %USERPROFILE%\Revisions\ghar\home\.vimrc
+
+PAUSE
