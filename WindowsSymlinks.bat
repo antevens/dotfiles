9536855f771fe9ab03e4d257377ab89445b39d7a
Antonia Stevens <a@ant.st>
2021-06-14 14:26:34 +0000

    Remove Ghar

--- WindowsSymlinks.bat
+++
@@ -1,6 +0,0 @@
-REM echo %USERPROFILE%
-mklink /D %USERPROFILE%\.vim %USERPROFILE%\Revisions\ghar\home\.vim
-mklink /D %USERPROFILE%\vimfiles %USERPROFILE%\Revisions\ghar\home\.vim
-mklink %USERPROFILE%\.vimrc %USERPROFILE%\Revisions\ghar\home\.vimrc
-
-PAUSE
